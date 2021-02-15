990c6138677090ebff5fea72432e67ce55297f33
astronomike <mikedsarkis@gmail.com>
2021-02-15 20:59:42 +0200

    initial commit

---
+++ translate.py
@@ -0,0 +1,31 @@
+class letter:
+    
+    __init__(self):
+        self.name = None
+        self.diacritic = None
+        self.position = None
+        
+
+def translate_line(eng_str):
+    n = len(eng_str)
+    for i in range(n):
+        arb_str[i] = eng_str[i]
+        
+    return arb_str
+
+def translate_file(infile):
+    try open(infile,r) as infile:
+        while not EoF:
+            #readline
+            #strip
+            numlines += 1
+    except: 
+        print("file does not exist")
+        
+    for i in range(numlines)
+#%%
+"""
+######################################################################
+Execution
+"""
+
